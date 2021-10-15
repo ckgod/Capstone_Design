64f970a65bc749ed5a8035d1f77977db0355ce2e
ckgod <rhckdrnr123@gmail.com>
2021-10-15 16:10:41 +0900

    bottom navigation set

--- app/src/main/java/com/ckg/appletree/MainActivity.kt
+++ app/src/main/java/com/ckg/appletree/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(R.id.homeMain, R.id.sellMain, R.id.profileMain)
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
