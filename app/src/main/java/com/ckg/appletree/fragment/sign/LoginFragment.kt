48ad381c997dc4f4756bbde413a4f708e7c41eb0
ckgod <rhckdrnr123@gmail.com>
2021-10-19 19:56:57 +0900

    login layout

---
+++ app/src/main/java/com/ckg/appletree/fragment/sign/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.ckg.appletree.fragment.sign
+
+import com.ckg.appletree.R
+import com.ckg.appletree.base.BaseKotlinFragment
+import com.ckg.appletree.databinding.FragmentLoginBinding
+import com.ckg.appletree.databinding.FragmentMainBinding
+
+class LoginFragment() : BaseKotlinFragment<FragmentLoginBinding>() {
+    override val layoutResourceId: Int
+        get() = R.layout.fragment_login
+
+    override fun initStartView() {
+
+    }
+
+    override fun initDataBinding() {
+    }
+
+    override fun initAfterBinding() {
+    }
+
+    override fun reLoadUI() {
+    }
+
+    companion object {
+        const val TAG = "LoginFragment"
+    }
+
+}
