1efc5407af01bf854a8cb0b1be32a89e3283fb09
ckgod <rhckdrnr123@gmail.com>
2021-11-01 16:48:35 +0900

    add product list

--- app/src/main/java/com/ckg/appletree/fragment/home/ProductListFragment.kt
+++ app/src/main/java/com/ckg/appletree/fragment/home/ProductListFragment.kt
@@ -2,22 +2,35 @@ package com.ckg.appletree.fragment.home
 
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ckg.appletree.R
 import com.ckg.appletree.base.BaseKotlinFragment
 import com.ckg.appletree.databinding.FragmentProductListBinding
 import com.ckg.appletree.databinding.FragmentTmpBinding
+import com.ckg.appletree.fragment.zAdapter.ProductAdapter
+import com.ckg.appletree.fragment.zItem.ProductItem
 
 class ProductListFragment() : BaseKotlinFragment<FragmentProductListBinding>() {
     override val layoutResourceId: Int
         get() = R.layout.fragment_product_list
 
     private val safeArgs : ProductListFragmentArgs by navArgs()
+    lateinit var productList : MutableList<ProductItem>
 
     override fun initStartView() {
         binding.tvTitle.text = safeArgs.categoryName
         binding.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
+        productList = mutableListOf(ProductItem(1,R.drawable.dummy_1,"dumm","dadf","adfa"),
+            ProductItem(2,R.drawable.dummy_1,"dumm","dadf","adfa"),
+            ProductItem(3,R.drawable.dummy_1,"dumm","dadf","adfa"),
+            ProductItem(4,R.drawable.dummy_1,"dumm","dadf","adfa"),
+            ProductItem(4,R.drawable.dummy_1,"dumm","dadf","adfa"),
+            ProductItem(4,R.drawable.dummy_1,"dumm","dadf","adfa"),
+            ProductItem(4,R.drawable.dummy_1,"dumm","dadf","adfa"))
+        binding.rvProduct.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
+        binding.rvProduct.adapter = ProductAdapter(requireActivity(),requireContext(),productList)
     }
 
     override fun initDataBinding() {
---
+++ app/src/main/java/com/ckg/appletree/fragment/zAdapter/ProductAdapter.kt
@@ -0,0 +1,53 @@
+package com.ckg.appletree.fragment.zAdapter
+
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.ckg.appletree.R
+import com.ckg.appletree.databinding.ItemCategoryBinding
+import com.ckg.appletree.databinding.ItemProductBinding
+import com.ckg.appletree.fragment.home.HomeMainFragmentDirections
+import com.ckg.appletree.fragment.zItem.CategoryItem
+import com.ckg.appletree.fragment.zItem.ProductItem
+import com.ckg.appletree.utils.ViewUtils
+
+class ProductAdapter(private val activity : Activity, private val context : Context, private val items : MutableList<ProductItem>) : RecyclerView.Adapter<ProductAdapter.ProductVH>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductVH {
+        val itemBinding = ItemProductBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ProductVH(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: ProductVH, position: Int) {
+        val item: ProductItem = items[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    inner class ProductVH(var binding: ItemProductBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ProductItem) {
+            Glide.with(context).load(item.image)
+                .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(ViewUtils.convertDpToPixel(8f,context).toInt())))
+                .into(binding.ivImage)
+        }
+    }
+
+    companion object {
+        const val TAG = "ProductAdapter"
+        const val MAC = 1
+        const val IMAC = 2
+        const val IPHONE = 3
+        const val WATCH = 4
+        const val IPAD = 5
+        const val ETC = 6
+    }
+
+}
