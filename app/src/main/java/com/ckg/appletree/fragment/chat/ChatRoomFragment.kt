97fbf8ea9c131bf52f4108da0d4841182aa30400
ckgod <rhckdrnr123@gmail.com>
2021-11-26 17:26:10 +0900

    add chat recyclerview

--- app/src/main/java/com/ckg/appletree/fragment/chat/ChatRoomFragment.kt
+++ app/src/main/java/com/ckg/appletree/fragment/chat/ChatRoomFragment.kt
@@ -1,21 +1,27 @@
 package com.ckg.appletree.fragment.chat
 
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ckg.appletree.R
 import com.ckg.appletree.base.BaseKotlinFragment
 import com.ckg.appletree.databinding.FragmentChatRoomBinding
+import com.ckg.appletree.fragment.zAdapter.MessageAdapter
+import com.ckg.appletree.fragment.zItem.TextMessage
 
 class ChatRoomFragment() : BaseKotlinFragment<FragmentChatRoomBinding>() {
     override val layoutResourceId: Int
         get() = R.layout.fragment_chat_room
 
     private val viewModel by lazy { ChatViewModel() }
+    lateinit var messageList : MutableList<TextMessage>
 
     override fun initStartView() {
         binding.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
-        viewModel.runStomp()
+        setDummy()
+        binding.rvChat.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.rvChat.adapter = MessageAdapter(requireActivity(),requireContext(),messageList)
     }
 
     override fun initDataBinding() {
@@ -27,6 +33,14 @@ class ChatRoomFragment() : BaseKotlinFragment<FragmentChatRoomBinding>() {
     }
 
     override fun reLoadUI() {
+    }
+
+    fun setDummy() {
+        messageList = mutableListOf(
+            TextMessage("고창국","1","1","강정훈",12,"hihi",true,true),
+            TextMessage("강정훈","1","1","고창국",12,"hihi",false,true),
+            TextMessage("고창국","1","1","강정훈",12,"hihi",true,true),
+            TextMessage("강정훈","1","1","고창국",12,"hihi",false,true),)
     }
 
     companion object {
--- app/src/main/java/com/ckg/appletree/fragment/chat/ChatViewModel.kt
+++ app/src/main/java/com/ckg/appletree/fragment/chat/ChatViewModel.kt
@@ -10,51 +10,7 @@ import ua.naiksoftware.stomp.dto.LifecycleEvent
 import ua.naiksoftware.stomp.dto.StompHeader
 
 class ChatViewModel :ViewModel() {
-    val jwtToken: String? = ApplicationClass.spToken?.getString(ApplicationClass.X_ACCESS_TOKEN, null)
-    // val url = "http://example.com:8080/"
-    var url = "ws://15.165.77.58:80/stomp/chat/websocket"
-    val stompClient =  Stomp.over(Stomp.ConnectionProvider.OKHTTP, url)
 
-    @SuppressLint("CheckResult")
-    fun runStomp(){
-        stompClient.connect()
-
-//        val headerList = arrayListOf<StompHeader>()
-//        headerList.add(StompHeader("chatRoomId","1"))
-//        headerList.add(StompHeader("sender", "ckgod"))
-//        headerList.add(StompHeader("content", "tesetsetsteset"))
-
-        stompClient.topic("/subscribe/stomp/message").subscribe { topicMessage ->
-            Log.d("message Recieve", topicMessage.payload)
-        }
-
-        stompClient.lifecycle().subscribe { lifecycleEvent ->
-            when (lifecycleEvent.type) {
-                LifecycleEvent.Type.OPENED -> {
-                    Log.d(TAG, "open")
-                }
-                LifecycleEvent.Type.CLOSED -> {
-                    Log.d(TAG, "close")
-
-                }
-                LifecycleEvent.Type.ERROR -> {
-                    Log.d(TAG, "error")
-                    Log.d(TAG, lifecycleEvent.exception.toString())
-                }
-                else ->{
-                    Log.d(TAG, "else $lifecycleEvent.message")
-                }
-            }
-        }
-
-//            val data = JSONObject()
-//            data.put("chatRoomId", "1")
-//            data.put("sender", "dasdf")
-//            data.put("content", "test")
-//
-//            stompClient.send("/publish/stomp/message", data.toString()).subscribe()
-
-    }
 
     companion object{
         const val TAG = "ChatViewModel"
